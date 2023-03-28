6675d7fe61ec465b680b8882df5ced372c2a06bf
uthoa1702 <uthoa1702@gmail.com>
2023-03-28 13:26:24 +0700

    bai tap a chanh giao

---
+++ module_2/ss19_regular_expression/src/valid_name/ValidFullName.java
@@ -0,0 +1,21 @@
+package valid_name;
+
+import java.util.Scanner;
+
+public class ValidFullName {
+    private static final String REGEX = "^[A-Z][a-z]*(\\s[A-Z][a-z]*)*\\s[A-Z][a-z]*$";
+
+    public static void main(String[] args) {
+        String name;
+        System.out.println("Enter class name:");
+        Scanner scanner = new Scanner(System.in);
+        name = scanner.nextLine();
+        boolean s = name.matches(REGEX);
+
+        if (s) {
+            System.out.println("valid class name");
+        } else {
+            System.out.println("invalid class name");
+        }
+    }
+}
