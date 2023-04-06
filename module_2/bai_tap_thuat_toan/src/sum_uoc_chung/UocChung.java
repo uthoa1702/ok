4c8bc2b87a59d6cf84e1312c17168669f2851f7d
uthoa1702 <uthoa1702@gmail.com>
2023-04-06 17:13:23 +0700

    Update UocChung.java

--- module_2/bai_tap_thuat_toan/src/sum_uoc_chung/UocChung.java
+++ module_2/bai_tap_thuat_toan/src/sum_uoc_chung/UocChung.java
@@ -13,7 +13,7 @@ public class UocChung {
         for (int i = 0; i < list1.size(); i++) {
             for (int j = 0; j < list2.size(); j++) {
                 if (list1.get(i) == list2.get(j)) {
-                    
+
                 }
             }
         }
